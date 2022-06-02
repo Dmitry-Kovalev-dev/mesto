9dbfe14cedb1963b89d359ed13d50f72dbfd5128
Dmitry Kovalev <doke174@yandex.ru>
2022-06-02 14:41:06 +0500

    5 sprint/2nd review

--- scripts/index.js
+++ scripts/index.js
@@ -4,18 +4,24 @@ const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__about');
 
 const popupProfile = document.querySelector('.popup_type_edit');
-const formElement = popupProfile.querySelector('.popup__form_type_profile-edit');
-const inputName = formElement.querySelector('.popup__input_type_name');
-const inputJob = formElement.querySelector('.popup__input_type_job');
+const formProfileElement = popupProfile.querySelector('.popup__form_type_profile-edit');
+const inputName = formProfileElement.querySelector('.popup__input_type_name');
+const inputJob = formProfileElement.querySelector('.popup__input_type_job');
 
 const popupAddPost = document.querySelector('.popup_type_add');
 const fomrAddElement = popupAddPost.querySelector('.popup__form_type_add-post');
 const inputPlaceName = fomrAddElement.querySelector('.popup__input_type_place');
 const inputLink = fomrAddElement.querySelector('.popup__input_type_link');
 
+const popupZoomImage = document.querySelector('.popup_type_img');
+const imageInPopup = popupZoomImage.querySelector('.popup__zoom-img');
+const captionInPopup = popupZoomImage.querySelector('.popup__caption');
+
 const closeButtons = document.querySelectorAll('.popup__close-btn');
 
 const photoFeed = document.querySelector('.photo-feed');
+
+const postTemplate = document.querySelector('#post').content;
 
 //---------------------open-popups---------------------------------------
 const openPopup = (popupElement) => {
@@ -34,82 +40,56 @@ addPostBtn.addEventListener('click', function () {
 });
 
 //-------------------close-popups---------------------------------------
-const closePopup = (evt) => {
-  evt.target.closest('.popup').classList.remove('popup_open');
+const closePopup = (popupElement) => {
+  popupElement.classList.remove('popup_open');
+};
+
+const handleClickClosePopup = (evt) => {
+  closePopup(evt.target.closest('.popup'));
 };
 
 closeButtons.forEach(button => {
-  button.addEventListener('click', closePopup);
+  button.addEventListener('click', handleClickClosePopup);
 });
 
 //-------------------edit-profile-submit--------------------------------
-const formSubmitHandler = (evt) => {
+const handleFormProfileSubmit = (evt) => {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileJob.textContent = inputJob.value;
-  closePopup(evt);
+  handleClickClosePopup(evt);
 };
 
-formElement.addEventListener('submit', formSubmitHandler);
+formProfileElement.addEventListener('submit', handleFormProfileSubmit);
 
 //----------------open-popup-zoom-img----------------------------------
-const popupImage = document.querySelector('.popup_type_img');
-
 const openPopupImage = (evt) => {
-  openPopup(popupImage);
-  popupImage.querySelector('.popup__zoom-img').src = evt.target.src;
-  popupImage.querySelector('.popup__caption').textContent =
-    evt.target.closest('.post').querySelector('.post__title').textContent;
+  openPopup(popupZoomImage);
+  imageInPopup.src = evt.target.src;
+  imageInPopup.alt = evt.target.closest('.post').querySelector('.post__title').textContent;
+  captionInPopup.textContent = evt.target.closest('.post').querySelector('.post__title').textContent;
 };
 
 //----------------------like-button-active-----------------------------
-const clickLikeButton = (evt) => {
+const handleClickLikeButton = (evt) => {
   evt.target.classList.toggle('post__like-btn_active');
 };
 
 //--------------------deleting-post------------------------------------
-const clickTrashButton = (evt) => {
+const handleClickTrashButton = (evt) => {
   evt.target.closest('.post').remove();
 };
 
 //-----------------------rendering-initial-cards-----------------------
-const initialCards = [
-  {
-    name: 'Сочнечная Долина',
-    link: './images/miass.jpg'
-  },
-  {
-    name: 'Архыз',
-    link: './images/arhyz.jpg'
-  },
-  {
-    name: 'Аджигардак',
-    link: './images/adzh.jpg'
-  },
-  {
-    name: 'Роза Хутор',
-    link: './images/rosa.jpg'
-  },
-  {
-    name: 'Губаха',
-    link: './images/gubaha.jpg'
-  },
-  {
-    name: 'Абзаково',
-    link: './images/abzakovo.jpg'
-  }
-];
-
-const postTemplate = document.querySelector('#post').content;
-
 const createPostCard = item => {
   const postElement = postTemplate.querySelector('.post').cloneNode(true);
+  const postImg = postElement.querySelector('.post__img');
   postElement.querySelector('.post__title').textContent = item.name;
-  postElement.querySelector('.post__img').alt = item.name || ' ';
-  postElement.querySelector('.post__img').src = item.link;
-  postElement.querySelector('.post__img').addEventListener('click', openPopupImage);
-  postElement.querySelector('.post__like-btn').addEventListener('click', clickLikeButton);
-  postElement.querySelector('.post__trash-btn').addEventListener('click', clickTrashButton);
+  postImg.alt = item.name || ' ';
+  postImg.src = item.link;
+  postImg.addEventListener('click', openPopupImage);
+  postElement.querySelector('.post__like-btn').addEventListener('click', handleClickLikeButton);
+  postElement.querySelector('.post__trash-btn').addEventListener('click', handleClickTrashButton);
   return postElement;
 };
 
@@ -119,7 +99,7 @@ initialCards.forEach(item => {
 });
 
 //----------------------added-new-post------------------------------------
-const formAddSubmitHandler = (evt) => {
+const handleFormAddSubmit = (evt) => {
   evt.preventDefault();
   const inputValue = {
     name: inputPlaceName.value,
@@ -127,8 +107,8 @@ const formAddSubmitHandler = (evt) => {
   };
   const newCard = createPostCard(inputValue);
   photoFeed.prepend(newCard);
-  closePopup(evt);
+  handleClickClosePopup(evt);
   fomrAddElement.reset();
 };
 
-fomrAddElement.addEventListener('submit', formAddSubmitHandler);
+fomrAddElement.addEventListener('submit', handleFormAddSubmit);
